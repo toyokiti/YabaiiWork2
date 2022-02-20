a52f717b7dfe70def2d00c8eacf12920fdae5096
toyokiti <akiakiakihiro0525@icloud.com>
2022-02-20 15:52:07 +0900

    2回目

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 'use strict';
 
-const fizzBuzz = (MultipleFizz, MultipleBuzz) => { //FizzBuzzの出力
-    const arrFizzBuzz = []; //FizzBuzzの結果を格納
+const fizzBuzz = (fizzNum, buzzNum) => { //FizzBuzzの出力
+    const results = []; //FizzBuzzの結果を格納
     for (let i = 1; i < 100; i++) {
         // Fizz＆Buzzの倍数のとき
-        if (i % MultipleFizz === 0 && i % MultipleBuzz === 0) {
+        if (i % fizzNum === 0 && i % buzzNum === 0) {
             const objFizzBuzz = { 'name': 'FizzBuzz', 'number': i };
-            arrFizzBuzz.push(objFizzBuzz);
-        } else if (i % MultipleFizz === 0) {
+            results.push(objFizzBuzz);
+        } else if (i % fizzNum === 0) {
             // Fizzの倍数のとき
             const objFizzBuzz = { 'name': 'Fizz', 'number': i };
-            arrFizzBuzz.push(objFizzBuzz);
-        } else if (i % MultipleBuzz === 0) {
+            results.push(objFizzBuzz);
+        } else if (i % buzzNum === 0) {
             // Buzzの倍数のとき
             const objFizzBuzz = { 'name': 'Buzz', 'number': i };
-            arrFizzBuzz.push(objFizzBuzz);
+            results.push(objFizzBuzz);
         }
     }
-    return arrFizzBuzz;
+    return results;
 }
 
-document.getElementById('submit').addEventListener('click', () => {
+document.getElementById('btn').addEventListener('click', () => {
     const fizzNum = document.getElementById('fizzNum'); //ユーザーが入力したFizzNumを格納
     const buzzNum = document.getElementById('BuzzNum'); //ユーザーが入力したBuzzNumを格納
     const outputArea = document.getElementById('outputArea'); //結果を出力するエリアのDOMを取得
@@ -28,13 +28,13 @@ document.getElementById('submit').addEventListener('click', () => {
 
         const arrFizzBuzz = fizzBuzz(fizzNum.value, buzzNum.value); //FizzBuzzを計算
         arrFizzBuzz.forEach((value) => {
-            const p = document.createElement('p');
-            p.textContent = `${value.name}: ${value.number}`;
-            outputArea.appendChild(p);
+            const li = document.createElement('li');
+            li.textContent = `${value.name}: ${value.number}`;
+            outputArea.appendChild(li);
         });
     } else { //ユーザーの入力が空白、または整数以外の場合
-        const p = document.createElement('p');
-        p.textContent = "整数を入力してください";
-        outputArea.appendChild(p);
+        const li = document.createElement('li');
+        li.textContent = '整数を入力してください';
+        outputArea.appendChild(li);
     }
 })
